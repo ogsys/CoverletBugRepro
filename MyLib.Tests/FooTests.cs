252e609aa85c11fb3ebcc057fa51405282c0b90e
Jeff Sharp <public@sharpjs.net>
2022-03-24 13:57:49 -0700

    Initial commit.

---
+++ MyLib.Tests/FooTests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace MyLib.Tests;
+
+[TestFixture]
+public class FooTests
+{
+    [Test]
+    public void Bar()
+    {
+        Foo.Bar();
+    }
+}
---
+++ MyLib/Foo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace MyLib;
+
+public static class Foo
+{
+    public static void Bar(SortOrder order = SortOrder.Ascending)
+    {
+        Console.WriteLine(order);
+    }
+}
